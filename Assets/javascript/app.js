ae211a418b245df9c97a87c106ff3ffee27f96b6
Josh Wood <wood2jp2@gmail.com>
2017-06-17 10:02:05 -0400

    commit, most functionality there, need to finish reset function and resetting timer

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,7 +1,8 @@
 $(document).ready( function() {
 	var timerRunning = false;
-	var intervalId;
-	var gameClock;
+	var	intervalId;
+	var timeoutId;
+	var	gameClock;
 		userScore = 0;
 		incorrectGuesses = 0;
 		questionDivCount = 0;
@@ -14,8 +15,8 @@ $(document).ready( function() {
 		option2Array = ["Jerry", "Islam", "Kramer", "Ho-Hos", "Bob Odenkirk", "San Francisco", "Apple Juice", "a bra", "gopher", "Young Masta"];
 		option3Array = ["Kramer", "Christianity", "The Landlord", "Snickers Bar", "Aaron Paul", "Chicago", "Ginger Ale", "A blouse", "golden retriever", "Lil Glock"];
 		option4Array = ["Newman", "Hinduism", "Bizarro Jerry", "Jelly Doughnuts", "Vince Gilligan", "New York City", "Grape Soda", "Heels", "flamingo", "Soda Poppa"];
-		divArray = [$('#option1'), $('#option2'), $('#option3'), $('#option4')];
-		var i = 0;
+		options = [$('#option1'), $('#option2'), $('#option3'), $('#option4')];
+		i = 0;
 
 
 	var triviaGame = {
@@ -43,9 +44,12 @@ $(document).ready( function() {
 	};
 		
 	$("#start").on('click', function() {
-			gameClock = 20;
+			gameClock = 1020;
 			timerRunning = true;
-
+			i=0;
+			// incorrectGuesses = 0;
+			// userScore = 0;
+			gameClock=20;
 			intervalId = setInterval(function() {
 				$('#timerDisplay').text(triviaGame.timeConverter(gameClock));
 				gameClock-=1;
@@ -53,52 +57,106 @@ $(document).ready( function() {
 					timerRunning = false;
 					clearInterval(intervalId);
 					alert("Times up!");
+					gameClock = 20;
+					i++;
+					incorrectGuesses++;
+					startGame();
 				}
 			}, 1000);
 			startGame();
 			$('#start').css("display", "none");
 	});
 
-	function startGame() {
+	$('#resetButton').on('click', function() {
+		i=0;
+		incorrectGuesses = 0;
+		userScore = 0;
+		timerRunning=true;
+		$('#gameDone').css("display", "none");
+		$('#resetGame').css("display", "none");
+		$('#question').css("display", "initial");
+		$('#option1').css("display", "initial");
+		$('#option2').css("display", "initial");
+		$('#option3').css("display", "initial");
+		$('#option4').css("display", "initial");
+			intervalId = setInterval(function() {
+				$('#timerDisplay').text(triviaGame.timeConverter(gameClock));
+				gameClock-=1;
+				if (gameClock===-1) {
+					timerRunning = false;
+					clearInterval(intervalId);
+					alert("Times up!");
+					gameClock = 20;
+					i++;
+					incorrectGuesses++;
+					startGame();
+				}
+			}, 1000);
+		startGame();
 
+	});
+
+	function questionArrayLoop (i) {
 		if (i<questionArray.length) {
 			$('#question').text(questionArray[i]);
 			$('#option1').text(correctAnswerArray[i]);
 			$('#option2').text(option2Array[i]);
 			$('#option3').text(option3Array[i]);
 			$('#option4').text(option4Array[i]);
-		};
+			console.log("Variable I is: " + i);
+			// var options = $('.incorrect');
+			// var correct = $('.correct');
+			// var index = Math.floor(Math.random() * 4);
 
-		$('.correct').on('click', function () {
-			alert("You are correct!");
-			userScore++;
-			gameClock = 20;
-			console.log(userScore);
-			i++;
-			startGame();
-		});
+			// options.splice(index, 0, correct);
+		} else {
+			timerRunning=false;
+			clearInterval(intervalId);
+			console.log("Game over");
+			$('#allOptions').css("display", "none");
+			$('#question').css("display", "none");
+			$('.option').css("display", "none");
+			$('#gameDone').css("display", "initial");
+			$('#correctCount').text(userScore);
+			$('#incorrectCount').text(incorrectGuesses);
+			$('#resetGame').css("display", 'initial');
+		}
+	};
 
-		$('.incorrect').on('click', function() {
-			alert("You are incorrect!");
-			incorrectGuesses++;
-			gameClock = 20;
-			i++;
-			startGame();
-		});	
+	function startGame() {
+		questionArrayLoop(i);
+
+			$('.correct').on('click', function () {
+				userScore++;
+				gameClock = 20;
+				console.log(userScore);
+				$('#allOptions').css("display", "none");
+				$('#correctDiv').css("display", "initial")
+				function timeoutId() {
+					$('#correctDiv').css("display", "none");
+					$('#allOptions').css("display", "initial");
+				};
+				var timeoutInteger = setTimeout(timeoutId, 1000);
+				i++;
+				questionArrayLoop(i);
+			});
+
+			$('.incorrect').on('click', function() {
+				incorrectGuesses++;
+				gameClock = 20;
+				console.log("Incorrect guesses are: " + incorrectGuesses);
+				$('#correctAnswer').text(correctAnswerArray[i]);
+				$('#allOptions').css("display", "none");
+				$('#incorrectDiv').css("display", "initial");
+				function timeoutId() {
+					$('#incorrectDiv').css("display", "none");
+					$('#allOptions').css("display", "initial");
+				};
+				var timeoutInteger = setTimeout(timeoutId, 1000);
+				i++;
+				questionArrayLoop(i);
+			});
+		
+		
 	}
-
-	// startGame();
-
-	// $('.correct1').on('click', function() {
-	// 	userScore++;
-	// 	$('#q1').css("display", "none");
-	// 	$('#q2').css("display", "initial");
-	// });
-
-
-	// $('.incorrect1').on('click', function() {
-	// 	incorrectGuesses++;
-	// 	$('#q1').css("display", "none");
-	// 	$('#q2').css("display", "initial");
-	// })
 });
